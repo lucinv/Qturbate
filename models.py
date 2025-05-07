04810f4f8f318b650fd77a3d9cf67e894420e884
Luc <luc.et.inversement+Qturbate@gmail.com>
2025-05-07 22:20:18 +0200

    plop

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class Video(BaseModel):
     thumbnail_url: HttpUrl  # Validation de format d'URL
     stream_url: HttpUrl
     # mpv_url c'est stream_url mais pour le lecteur mpv : on remplace "https://" par "mpv://"
-    mpv_url: str = Field(..., example="mpv://example.com/video")  # Champs obligatoire avec exemple
+    mpv_url: str = None
     description: str = None  # Champs optionnel
     tags: list = []
 
