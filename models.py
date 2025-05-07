18b923a6485e87b86f66516a42502cf6e53c3a1d
Luc <luc.et.inversement+Qturbate@gmail.com>
2025-05-07 22:16:33 +0200

    mpv_url

--- models.py
+++ models.py
@@ -6,8 +6,15 @@ class Video(BaseModel):
     title: str = Field(..., example="A Great Video")  # Champs obligatoire avec exemple
     thumbnail_url: HttpUrl  # Validation de format d'URL
     stream_url: HttpUrl
+    # mpv_url c'est stream_url mais pour le lecteur mpv : on remplace "https://" par "mpv://"
+    mpv_url: str = Field(..., example="mpv://example.com/video")  # Champs obligatoire avec exemple
     description: str = None  # Champs optionnel
     tags: list = []
+
+    # fonction d'initialisation ù on crée l'url mpv_url
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.mpv_url = self.stream_url.replace("https://", "mpv://")
 
 db = SQLAlchemy()
 
